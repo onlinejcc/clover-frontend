2e5a8f4bda14efdf5e503c1bc7b4c06b44efd501
Gifairu <gifairu@gmail.com>
2021-11-18 02:58:44 +0700

    Add content feature

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import Header from './assets/header.png';
+import Frame1 from './assets/frame1.png';
+import Frame2 from './assets/frame2.png';
+import Frame3 from './assets/frame3.png';
 
 const Home = () => {
   return (
@@ -25,6 +28,63 @@ const Home = () => {
         </div>
         <div className="flex justify-end w-1/2">
           <img src={Header} alt="Header" />
+        </div>
+      </div>
+
+      <div className="rounded-md mx-16 my-24" style={{ backgroundColor: '#F3F8EE' }}>
+        <h1
+          className="font-primary font-bold text-4xl w-1/2 text-center leading-tight pt-28 m-auto"
+          style={{ color: '#3D655F' }}
+        >
+          Pilihan terbaik untuk makanan organik.
+        </h1>
+        <div className="flex justify-center pt-14 pb-28 space-x-12">
+          <div className="flex items-center">
+            <img src={Frame1} alt="Produk" />
+            <div>
+              <span>
+                <strong style={{ color: '#3D655F' }}>50+</strong>
+                <p style={{ color: '#ACAEAF' }}>Variasi produk</p>
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <img src={Frame2} alt="Mitra" />
+            <div>
+              <span>
+                <strong style={{ color: '#3D655F' }}>500K+</strong>
+                <p style={{ color: '#ACAEAF' }}>Mitra Kami</p>
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <img src={Frame3} alt="Negara" />
+            <div>
+              <span>
+                <strong style={{ color: '#3D655F' }}>100+</strong>
+                <p style={{ color: '#ACAEAF' }}>Negara pengekspor</p>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center mx-16">
+        <div className="flex items-center space-x-10">
+          <h1 className="font-primary font-bold text-xl" style={{ color: '#3D655F' }}>
+            Produk Mitra
+          </h1>
+          <p className="font-primary text-sm cursor-pointer hover:underline" style={{ color: '#3D655F' }}>
+            See all &rarr;
+          </p>
+        </div>
+        <div className="flex space-x-5">
+          <div className="text-white py-1 px-3" style={{ backgroundColor: '#D6D7D7' }}>
+            <p>&larr;</p>
+          </div>
+          <div className="text-white py-1 px-3" style={{ backgroundColor: '#7AB257' }}>
+            <p>&rarr;</p>
+          </div>
         </div>
       </div>
     </div>
